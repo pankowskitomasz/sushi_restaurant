7d85ed2c7acdbc5caf86b78f469e6418c62e7806
tomaszpankowski <--get-all>
2022-02-05 00:22:55 +0100

    layout modified

--- src/views/menu.js
+++ src/views/menu.js
@@ -1,11 +1,11 @@
 import React,{Component} from "react";
-
+import TitleTextTableCenter from "../components/title-text-table-center";
 
 class Menu extends Component{
     render(){
         return(          
-            <main className="bg-menu minh-footer-adj">     
-
+            <main className="bg-menu minh-footer-adj pt-5">     
+                <TitleTextTableCenter/>
             </main>
         );
     }
