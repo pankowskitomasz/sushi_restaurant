6a05f2d3830b198cdcad44370e4fc43c9413078f
tomaszpankowski <--get-all>
2022-02-06 12:34:26 +0100

    added backend

--- src/components/login-error.js
+++ src/components/login-error.js
@@ -5,7 +5,7 @@ import Col from "../../node_modules/react-bootstrap/Col";
 import Button from "../../node_modules/react-bootstrap/Button";
 import Card from "../../node_modules/react-bootstrap/Card";
 
-class MessageSent extends Component {
+class LoginError extends Component {
     hndBack(){
         this.props.backNav("form");
     }
@@ -14,14 +14,14 @@ class MessageSent extends Component {
             <Container fluid className={"login-form align-items-center p-0 py-5 bg-white d-flex minh-footer-adj " + this.props.classExt}>
                 <Row className="mx-auto text-center w-100">
                     <Col xs={10} md={6} className="mx-auto p-0 d-flex align-items-center">
-                        <Card className="mx-auto bg-form border-green shadow bg-form">
-                            <Card.Header className="text-white text-start bg-green border-green">
-                                Message sent!
+                        <Card className="mx-auto border border-green rounded bg-form shadow text-green">
+                            <Card.Header className="text-green fw-bold text-start">
+                                Login error!
                             </Card.Header>
-                            <Card.Body className="text-center text-green bg-form p-5">
-                                <p className="lead">Message was sent!</p>
-                                <Button variant={"outline-light border-green text-green rounded-pill"} onClick={this.hndBack.bind(this)}>
-                                    back to Form
+                            <Card.Body className="text-center text-secondary p-5">
+                                <p className="lead">Incorrect login or password!</p>
+                                <Button variant={"outline-light border-green text-green"} onClick={this.hndBack.bind(this)}>
+                                    OK
                                 </Button>
                             </Card.Body>
                         </Card>
@@ -32,4 +32,4 @@ class MessageSent extends Component {
     }
 }
 
-export default MessageSent;
+export default LoginError;
